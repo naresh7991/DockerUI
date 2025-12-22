3b6fa117b81d8331fc53f381caf4a136ac99a77f
YashSaresth <yashsingh53208@gmail.com>
2025-12-23 01:19:48 +0530

    fixing the build issue

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,6 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-  //add unused variable for checking build fail and blocking pr merge
-  const unusedVar = ""
 
   return (
     <>
